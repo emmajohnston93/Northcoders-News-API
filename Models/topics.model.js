cd185c44dd5bf1db98ebda1b3c117002893b47c7
Emma Johnston <emmajohnston@emmas-air.home>
2023-10-03 15:47:45 +0100

    kata 4 done

--- Models/topics.model.js
+++ Models/topics.model.js
@@ -9,7 +9,22 @@ function fetchTopics() {
     })
 }
 
+function fetchArticleByID(article_id) {
+
+  const query = 'SELECT * FROM articles WHERE article_id = $1;';
+  return db
+  .query(query, [article_id])
+  .then((res) => {
+  if(res.rows.length === 0) {
+  return Promise.reject({ status: 404, msg: 'Article with this ID not found'});  
+  } else {
+   return res.rows[0];
+  }
+  })
+}
+
+
 
    
 
-module.exports = { fetchTopics }
+module.exports = { fetchTopics, fetchArticleByID }
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getTopics, availableEndpoints, getEndpoints } = require('./Controllers/topics.controller');
+const { getTopics, getEndpoints, getArticleByID } = require('./Controllers/topics.controller');
 require('dotenv').config();
 const endpoints = require('./endpoints.json')
 
@@ -9,9 +9,22 @@ app.get('/api/topics', getTopics)
 
 app.get('/api', getEndpoints) 
 
+app.get('/api/articles/:article_id', getArticleByID)
+
+app.use((err, req, res, next) => {
+if(err.code === '22P02') {
+  res.status(400).send({msg: 'Search not possible - please use an id number' }) 
+} else {
+  next(err)
+}
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status).send({msg: err.msg});
+  })
+
 app.all('/*', (req, res, next) => {
  res.status(404).send({msg: 'Error 404 - Not Found'}) 
  })
-   
     
 module.exports = app
